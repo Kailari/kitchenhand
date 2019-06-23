aa0af52febbbab37cced43aa8aedc71c67218068
toilari <ilari97@hotmail.com>
2019-06-23 16:44:19 +0300

    Fixed JWT_SECRET and PORT being undefined by default on CI/Test environment

--- src/config.js
+++ src/config.js
@@ -2,19 +2,23 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.set('useFindAndModify', false)
-let dbUri, saltRounds
+let dbUri, saltRounds, jwtSecret, port
 if (process.env.NODE_ENV === 'test') {
+  port = 4000
   dbUri = global.__MONGO_URI__
+  jwtSecret = 'test_secret'
   saltRounds = 1
 } else {
+  port = process.env.PORT
+  jwtSecret = process.env.JWT_SECRET
   dbUri = process.env.DATABASE_URL
   saltRounds = 10
 }
 
 const SALT_ROUNDS = saltRounds
 const MONGODB_URI = dbUri
-const JWT_SECRET = process.env.JWT_SECRET
-const PORT = process.env.PORT
+const JWT_SECRET = jwtSecret
+const PORT = port
 
 module.exports = {
   MONGODB_URI,
