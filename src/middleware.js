da20e508a19aaf25adf42e27e36a8f42a8d151b9
toilari <ilari97@hotmail.com>
2019-06-23 19:19:52 +0300

    Forgot one next() so that https requests get forwarded to other middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,8 @@ const sslRedirect = (environments, status) => {
     if (environments.indexOf(process.env.NODE_ENV) >= 0) {
       if (req.headers['x-forwarded-proto'] != 'https') {
         res.redirect(status, `https://${req.hostname}${req.originalUrl}`)
+      } else {
+        next()
       }
     } else {
       next()
