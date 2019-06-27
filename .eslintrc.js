7ff79665bca066c9338e2fee9e5ccece85e27b87
toilari <ilari97@hotmail.com>
2019-06-27 09:05:32 +0300

    Configured eslint for TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,54 @@
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es6': true,
-        'node': true,
-        'jest': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'no-console': 0,
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        'multiline': {
+          'delimiter': 'comma',
+          'requireLast': true
+        },
+        'singleline': {
+          'delimiter': 'comma',
+          'requireLast': true
+        },
+      }
+    ],
+    "indent": "off",
+    '@typescript-eslint/indent': ['error', 2],
+    'no-console': 0,
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
 }
