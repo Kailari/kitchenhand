9526a4919bdd78a1e99f4ccd64042406cada71f2
toilari <ilari97@hotmail.com>
2019-07-18 13:10:12 +0300

    Added some queries/mutations for ingredients

--- src/graphql/types/ingredient.ts
+++ src/graphql/types/ingredient.ts
@@ -6,6 +6,16 @@ const types = gql`
     name: String! @column
     defaultUnit: Unit @link
   }
+
+  extend type Query {
+    getIngredient(id: ID!): Ingredient
+    findIngredient(filter: String!): [Ingredient!]!
+  }
+
+  extend type Mutation {
+    addIngredient(name: String!, defaultUnitId: ID!): Ingredient
+    removeIngredient(id: ID!): Ingredient
+  }
 `
 
 export default types
